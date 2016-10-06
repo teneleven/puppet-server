67e262675ed8ec69205b3837b9b137ff7f523b4d
Michael Mackus <michaelmackus@gmail.com>
2016-10-06 11:01:05 -0700

    Implement acls & exec

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,25 +3,54 @@ class server (
   $cron     = {},
   $groups   = [],
   $users    = {},
+
+  $exec          = {},
+  $exec_defaults = {
+    path => ['/usr/bin', '/bin', '/usr/sbin', '/sbin']
+  },
+
+  $acls         = {},
+  $default_acls = []
 ) {
   if ($packages) {
-    ::package { $packages: }
+    package { $packages: }
   }
 
   if ($cron) {
-    ::service { 'cron':
+    service { 'cron':
       ensure => $cron['install'],
       enable => $cron['install']
     }
 
-    create_resources('::cron', $cron['jobs'])
+    if ($cron['jobs']) {
+      create_resources('cron', $cron['jobs'])
+    }
   }
 
   if ($groups) {
-    ::group { $groups: }
+    group { $groups: }
   }
 
   if ($users) {
-    create_resources('::user', $users)
+    create_resources('user', $users)
+  }
+
+  $exec.each |$exe| {
+    create_resources('exec', { "server_exec_${exe}" => merge(
+      $exec_defaults,
+      { command => $exe }
+    ) })
+
+    /* TODO ??? */
+    /* if (!empty($packages)) { */
+    /*   Class['::teneleven::apt'] -> Exec["server_exec_${exe}"] */
+    /* } */
+  }
+
+  $acls.each |$file, $permissions| {
+    create_resources('::fooacl::conf', {"server_acl_${file}" => {
+      target      => $file,
+      permissions => concat($default_acls, $permissions)
+    }})
   }
 }
