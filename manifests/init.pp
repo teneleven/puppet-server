4d18e261f6b19d72176d7c5ba7e5111d9d5c9679
Michael Mackus <michaelmackus@gmail.com>
2017-02-16 10:30:42 -0800

    Enable configuration of classes & resources

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,11 +9,18 @@ class server (
     path => ['/usr/bin', '/bin', '/usr/sbin', '/sbin']
   },
 
-  $acls         = {},
-  $default_acls = []
+  $acls          = {},
+  $default_acls  = [],
+
+  # puppet classes to define - should be either list of classes, or FULL::CLASS as KEY and hash PARAMS as value
+  $classes       = {},
+  # puppet resources - should be FULL::RESOURCE as KEY and hash { NAME => PARAMS } as value
+  $resources     = {},
 ) {
-  if ($packages) {
+  if ($packages and is_array($packages)) {
     package { $packages: }
+  } elsif ($packages and is_hash($packages)) {
+    create_resources('package', $packages)
   }
 
   if ($cron) {
@@ -27,24 +34,26 @@ class server (
     }
   }
 
-  if ($groups) {
+  if ($groups and is_array($groups)) {
     group { $groups: }
+  } elsif ($groups and is_hash($groups)) {
+    create_resources('group', $groups)
   }
 
-  if ($users) {
+  if ($users and is_hash($users)) {
     create_resources('user', $users)
   }
 
-  $exec.each |$exe| {
-    create_resources('exec', { "server_exec_${exe}" => merge(
-      $exec_defaults,
-      { command => $exe }
-    ) })
-
-    /* TODO ??? */
-    /* if (!empty($packages)) { */
-    /*   Class['::teneleven::apt'] -> Exec["server_exec_${exe}"] */
-    /* } */
+  # TODO setup relationship(s) between exec and $packages
+  if ($exec and is_array($exec)) {
+    $exec.each |$exe| {
+      create_resources('exec', { "server_exec_${exe}" => merge(
+        $exec_defaults,
+        { command => $exe }
+      ) })
+    }
+  } elsif ($exec and is_hash($exec)) {
+    create_resources('exec', $exec)
   }
 
   $acls.each |$file, $permissions| {
@@ -52,5 +61,19 @@ class server (
       target      => $file,
       permissions => concat($default_acls, $permissions)
     }})
+  }
+
+  if ($classes and is_array($classes)) {
+    $classes.each |$class| {
+      include $class
+    }
+  } elsif ($classes and is_hash($classes)) {
+    create_resources('class', $classes)
+  }
+
+  if ($resources and is_hash($resources)) {
+    $resources.each |$resource, $params| {
+      create_resources($resource, $params)
+    }
   }
 }
